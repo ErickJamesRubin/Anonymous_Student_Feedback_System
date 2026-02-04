efa74247516c42c351a5270b0a08689f2061efa4
ranssyyy <ranscandy9@gmail.com>
2026-02-04 13:01:23 +0800

    Added visibility buttons

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ function togglePassword() {
   password.type = password.type === "password" ? "text" : "password";
 }
 
+ const toggleOptions = document.querySelectorAll(".toggle-option");
+  const visibilityInput = document.getElementById("visibilityInput");
+
+  toggleOptions.forEach(option => {
+    option.addEventListener("click", () => {
+      
+      toggleOptions.forEach(o => o.classList.remove("active"));
+
+      
+      option.classList.add("active");
+
+      
+      visibilityInput.value = option.dataset.value;
+    });
+  });
+ 
+
 document.getElementById("loginForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
